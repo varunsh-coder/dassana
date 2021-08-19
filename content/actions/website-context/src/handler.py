70ab62dafec638abfd66fc6aef6d4116aed70329
dassana-junix <68706746+dassana-junix@users.noreply.github.com>
2021-08-19 15:43:47 -0700

    feat: Website context action (#119)

--- content/actions/website-context/src/handler.py
+++ content/actions/website-context/src/handler.py
@@ -3,23 +3,30 @@ from typing import Dict, Any, Optional
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.validation import validator
+from boto3 import resource
 from botocore.exceptions import ClientError
+from aws_lambda_powertools import Logger
 
-from dassana.common.aws_client import DassanaAwsObject
+from dassana.common.aws_client import DassanaAwsObject, parse_arn
 
 with open('input.json', 'r') as schema:
     schema = load(schema)
-    dassana_aws = DassanaAwsObject(
-        schema['properties']['awsRegion']['default'])
+    dassana_aws = DassanaAwsObject()
+
+logger = Logger(service='dassana-actions')
 
 
+@logger.inject_lambda_context
 @validator(inbound_schema=schema)
 def handle(event: Dict[str, Optional[Any]], context: LambdaContext):
-    client = dassana_aws.create_aws_client(event, context, 's3')
+    arn = parse_arn(event.get('bucketArn'))
+    region = event.get('awsRegion')
+    arn_resource = arn.get('resource')
+    client = dassana_aws.create_aws_client(context, 's3', event.get('awsRegion'))
+
     try:
-        resp = client.get_bucket_website(
-            Bucket=event['bucket'],
-        )
-        return True
+        bucket_website = client.get_bucket_website(Bucket=arn_resource)
+
+        return {"bucketUrl": "%s.s3-website-%s.amazonaws.com" % (arn_resource, region)}
     except ClientError as e:
-        return False
+        logger.exception(e)
