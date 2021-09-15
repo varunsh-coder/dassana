056e0f48129462a922861029a68fa943f61dd2d7
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-09-15 12:53:49 -0700

    Update docs theme (#237)

--- docs-root/docusaurus.config.js
+++ docs-root/docusaurus.config.js
@@ -19,6 +19,9 @@ module.exports = {
 		}
 	],
 	themeConfig: {
+		prism: {
+			theme: require('prism-react-renderer/themes/nightOwl')
+		},
 		colorMode: {
 			defaultMode: 'dark',
 			disableSwitch: true
