32b4b6b30debbbf663c42c03cb9f17bc7aec0c2a
Adam Kelly <adam@Adams-MacBook-Pro.local>
2021-09-20 11:12:08 -0700

    reset App.java to beginning state

--- engine/src/main/java/app/dassana/core/launch/App.java
+++ engine/src/main/java/app/dassana/core/launch/App.java
@@ -2,76 +2,18 @@ package app.dassana.core.launch;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import software.amazon.awssdk.utils.IoUtils;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import static app.dassana.core.contentmanager.ContentManager.WORKFLOW_ID;
-import static app.dassana.core.launch.ApiHandler.*;
-import static app.dassana.core.launch.ApiHandler.API_INCLUDE_ALERT_IN_OUTPUT;
+import software.amazon.awssdk.utils.IoUtils;
 
 public class App {
 
-  static APIGatewayProxyRequestEvent getApiGatewayProxyRequestEvent(Map<String, String> parameters) {
-    APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent = new APIGatewayProxyRequestEvent();
-    if (parameters != null && parameters.size() > 0) {
-      apiGatewayProxyRequestEvent.setQueryStringParameters(parameters);
-    }
-    apiGatewayProxyRequestEvent.setIsBase64Encoded(false);
-    return apiGatewayProxyRequestEvent;
-  }
-
-  static Map<String, String> getQueryParams() {
-    Map<String, String> queryParams = new HashMap<>();
-    queryParams.put(API_PARAM_SKIP_GENERAL_CONTEXT, "false");
-    queryParams.put(API_PARAM_SKIP_POLICY_CONTEXT, "false");
-    queryParams.put(API_PARAM_SKIP_POST_PROCESSOR, "true");
-    queryParams.put(API_PARAM_SKIP_S3UPLOAD, "true");
-    queryParams.put(API_INCLUDE_ALERT_IN_OUTPUT, "false");
-
-    return queryParams;
-  }
-
-  static APIGatewayProxyRequestEvent getResponse(String workflowId, String method, boolean isDefault) throws Exception {
-    Map<String, String> queryParams = getQueryParams();
-    queryParams.put(WORKFLOW_ID, workflowId);
-    if(isDefault){
-      queryParams.put("default", "true");
-    }
-
-    APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent = getApiGatewayProxyRequestEvent(queryParams);
-    apiGatewayProxyRequestEvent.setHttpMethod(method);
-    apiGatewayProxyRequestEvent.setPath(WORKFLOW_PATH);
-
-    return apiGatewayProxyRequestEvent;
-  }
-
-  static void testApi(){
-    try {
-      ApiHandler apiHandler = new ApiHandler();
-
-      boolean isDefault = false;
-      isDefault = true;
-
-      String workflowId = "foo-cloud-normalize";
-      //workflowId = "general-context-demo-cloud";
-      //workflowId = "demo-cloud-resource-context";
-
-      APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent = getResponse(workflowId, "delete", isDefault);
-      APIGatewayProxyResponseEvent responseEvent = apiHandler.execute(apiGatewayProxyRequestEvent);
-      String body = responseEvent.getBody();
-      System.out.println(body);
-    }catch (Exception e){
-      e.getStackTrace();
-    }
-  }
-
   public static void main(String[] args) throws IOException {
-    //testApi();/*
+
     ApiHandler apiHandler = new ApiHandler();
     String event = IoUtils
-        .toUtf8String(Thread.currentThread().getContextClassLoader().getResourceAsStream("TestEvent.json"));
+            .toUtf8String(Thread.currentThread().getContextClassLoader().getResourceAsStream("TestEvent.json"));
     APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent = new APIGatewayProxyRequestEvent();
 
     apiGatewayProxyRequestEvent.setBody(event);
@@ -94,7 +36,6 @@ public class App {
     } else {
       System.exit(0);
     }
-    //*/
   }
 
 }
