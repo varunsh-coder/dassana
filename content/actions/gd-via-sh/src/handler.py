db5e63d72c0d4091358eae989ab4c3107dcb1b61
Hossein Khosravani <Hosein@dassana.io>
2021-10-05 22:05:28 -0700

    testing

--- content/actions/gd-via-sh/src/handler.py
+++ content/actions/gd-via-sh/src/handler.py
@@ -1,5 +1,6 @@
 from json import loads, load
 from typing import Dict, List, Any, Optional
+from pydantic import BaseModel
 
 from aws_lambda_powertools.utilities.parser import event_parser, parse
 from aws_lambda_powertools.utilities.typing import LambdaContext
@@ -8,15 +9,47 @@ from dassana.common.aws_client import parse_arn
 from dassana.common.models import NormalizedOutput
 from dassana.common.aws_client import LambdaTestContext
 
-from normalize.models import GuardDutyAlert
-from normalize.models import GuardDutyDirect
+
+class Resources(BaseModel):
+    Partition: str = None
+    Type: str = None
+    Region: str = None
+    Id: str = None
 
 
-# @event_parser(model=GuardDutyAlert)
-# def handle(event: GuardDutyAlert, context: LambdaContext):
+class Detail(BaseModel):
+    findings: List[Dict[Any, Any]] = None
+
+
+class GuardDutyAlert(BaseModel):
+    Id: str = None
+    ProductArn: str = None
+    Region: str = None
+    AwsAccountId: str = None
+    Types: List[str] = []
+    ProductFields: Optional[Dict[str, str]] = None
+    Resources: Optional[List[Resources]]
+    detail: Detail = None
+
+
+class DirectResource(BaseModel):
+    accessKeyDetails: Optional[Dict[str, str]]
+    s3BucketDetails: Optional[List[Dict[Any, Any]]]
+    instanceDetails: Optional[Dict[Any, Any]]
+
+
+class GuardDutyDirect(BaseModel):
+    accountId: str = None
+    region: str = None
+    type: str = None
+    resource: DirectResource = None
+    arn: str = None
+    id: str = None
+
+
 def handle(event, context: LambdaContext):
-    mode = 'n' # n --> normal - we default to raw and event bridge alert format
-    if isinstance(event, list): # direct GuardDuty alert with have [{}] format
+    mode = 's' # s --> SecurityHub - we default to raw and event bridge alert format
+    if isinstance(event, list): # direct GuardDuty alert's has [{}] format
         print("it is list")
         event = parse(event[0], model=GuardDutyDirect)
         mode = 'd' # --> since direct alert have different tag names we need flag to distinguish
@@ -27,77 +60,61 @@ def handle(event, context: LambdaContext):
         raise TypeError("ERROR: input file is not of type List or Dictionary. Please Check.")
 
     # only EventBridge Alert has tags .event.detail.findings[]
-    if (mode=='n') and (event.detail is not None):
+    if (mode=='s') and (event.detail is not None):
         event = parse(event.detail.findings[0], model=GuardDutyAlert)
 
-    policy_id = event.Types[0] if mode=='n' else event.type
-    # if alert is direct we can set the resource since when there is a matching type
+    policy_id = event.Types[0] if mode=='s' else event.type
+
     if 'ec2' in policy_id.lower(): # EC2
-        rt = 'AwsEc2Instance' if mode=='n' else 'instanceDetails'
-        if mode=='d': resource = event.resource.instanceDetails
-        service = "ec2"
+        rt = 'AwsEc2Instance' if mode=='s' else 'instanceDetails'  # direct alerts have different resource name
+        if mode=='d': resource = event.resource.instanceDetails  # direct alerts have resource type as a tag
+        service = "ec2"  # we don't need to wait for policy context to set this value since we have the policyId
         resource_type = "instance"
     elif 'iam' in policy_id.lower(): # IAM
-        rt = 'AwsIamAccessKey' if mode=='n' else 'accessKeyDetails'
+        rt = 'AwsIamAccessKey' if mode=='s' else 'accessKeyDetails'
         if mode == 'd': resource = event.resource.accessKeyDetails
         service = "iam"
         resource_type = "user"
     elif 's3' in policy_id.lower(): # S3
-        rt = 'AwsS3Bucket' if mode=='n' else 's3BucketDetails'
+        rt = 'AwsS3Bucket' if mode=='s' else 's3BucketDetails'
         if mode == 'd': resource = event.resource.s3BucketDetails
         service = "s3"
-        resource_type = "user"
+        resource_type = "bucket"
     else:
         rt = None
 
-    # throws an exception if there were no matching resource with event type
+    # throws an exception if there were no resource matching with policyId
     if rt is None:
         raise ValueError("ERROR: There were no matching resource with type " + rt)
 
-    if mode=='n': # resources is List in case of Security Hub,
-        for resource in event.Resources:
+    if mode=='s': # resources is List in case of Security Hub
+        for resource in event.Resources:  # traverse the resources list and match with type from policyId
             if resource.Type == rt:
                 break
 
-    # only Alerts from SecurityHub and s3BucketDetails from GuardDuty direct have arn tags
-    resource_arn = resource.Id if (mode=='n') else event.resource.s3BucketDetails['arn'] if rt=='s3BucketDetails' else ''
+    # instanceDetails and accessKeyDetails resources from GuardDuty direct do not have arn
+    resource_arn = resource.Id if mode=='s' else event.resource.s3BucketDetails['arn'] if rt=='s3BucketDetails' else ''
 
     arn_obj = parse_arn(resource_arn) if not resource_arn=='' else None
     if (arn_obj is not None and arn_obj.resource_type):
         resource_id = arn_obj.resource
     else:
-        if mode=='n':
+        if mode=='s':
             resource_info = resource_arn.split(":")[5]
             resource_id = (resource_info.split("/")[2]) if ('/' in resource_info) else resource_info
         else: # direct alert has unique tags for each resource type
-            if rt=='instanceDetails': resource_id = event.resource.instanceDetails['instanceId']
-            elif rt=='accessKeyDetails': resource_id = event.resource.accessKeyDetails['accessKeyId']
-            else: resource_id = event.resource.s3BucketDetails['name']
+            if rt=='instanceDetails': resource_id = event.resource.instanceDetails['instanceId']  # EC2
+            elif rt=='accessKeyDetails': resource_id = event.resource.accessKeyDetails['accessKeyId']  # IAM
+            else: resource_id = event.resource.s3BucketDetails['name']  # S3
 
     # many times ARNs do haven't have region e.g. s3 bucket. so we rely on what the finding value is
-    if (mode=='n'):
+    if (mode=='s'):  # raw and eventBridge
         region = arn_obj.region if arn_obj.region else (resource.Region if resource.Region else None)
     else:
-        region = event.region
+        region = event.region  # direct
 
-    alert_id = event.Id if mode=='n' else event.arn
-    resource_container = event.AwsAccountId if mode=='n' else event.accountId
-    policy_id = policy_id
-    csp = "aws"
-    vendor_id = "guardduty"
-    resource_region = region
-
-    # print('+++csp: ' + csp)
-    # print("+++resource_container: " + resource_container)
-    # print("+++region: " + resource_region)
-    # print("+++resourceId: " + resource_id)
-    # print("+++alertId: " + alert_id)
-    # if resource_arn is not None: print("+++arn: " + resource_arn)
-    # print("+++vendorPolicy: " + policy_id)
-    # print("+++vendorId: " + vendor_id)
-    # print("+++service: " + service)
-    # print("+++resourceType: " + resource_type)
-    # print("+++rt: " + rt)
+    alert_id = event.Id if mode=='s' else event.arn
+    resource_container = event.AwsAccountId if mode=='s' else event.accountId
 
     output = NormalizedOutput(
         csp='aws',
@@ -109,16 +126,7 @@ def handle(event, context: LambdaContext):
         resourceType=resource_type,
         service=service,
         vendorPolicy=policy_id,
-        vendorId='aws-config'
+        vendorId='guardduty'
     )
-
-    # print('+++csp: ' + csp)
-    # print("+++resource_container: " + resource_container)
-    # print("+++region: " + resource_region)
-    # print("+++resourceId: " + resource_id)
-    # print("+++alertId: " + alert_id)
-    # print("+++arn: " + resource_arn)
-    # print("+++vendorPolicy: " + policy_id)
-    # print("+++vendorId: " + vendor_id)
 
     return loads(output.json())
--- content/actions/gd-via-sh/src/test_handler.py
+++ content/actions/gd-via-sh/src/test_handler.py
@@ -35,12 +35,12 @@ class GuardDutyNormalizerTest(TestCase):
         print("+++service: " + norm.service)
         print("+++resourceType: " + norm.resourceType)
 
-    # def test_eventbridge(self):
-    #     with open('gd-via-sh/tests/examples/eb-ec2.json') as f:
-    #         alert = load(f)
-    #         resp = handle(alert, LambdaTestContext('test-guardduty-normalizer'))
-            # norm = NormalizedOutput(**resp)
-            # self.print_pretty(norm, 'eb-ec2.json')
+    def test_eb_ec2(self):
+        with open('gd-via-sh/tests/examples/eb-ec2.json') as f:
+            alert = load(f)
+            resp = handle(alert, LambdaTestContext('test-guardduty-normalizer'))
+            norm = NormalizedOutput(**resp)
+            self.print_pretty(norm, 'eb-ec2.json')
             # assert norm.vendorId == 'prisma-cloud'
             # assert norm.alertId == 'P-2388888'
             # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
@@ -49,12 +49,12 @@ class GuardDutyNormalizerTest(TestCase):
             # assert norm.region == 'ap-northeast-1'
             # assert norm.resourceContainer == '123456789123'
 
-    # def test_raw_s3_multiple_resource(self):
-        # with open('gd-via-sh/tests/examples/raw-s3-multiple-resource.json') as f:
-        #     alert = load(f)
-        #     resp = handle(alert, LambdaTestContext('test-guardduty-normalizer'))
-    #         norm = NormalizedOutput(**resp)
-    #         self.print_pretty(norm, 'gd-raw.json')
+    def test_raw_s3_multiple_resource(self):
+        with open('gd-via-sh/tests/examples/raw-s3-multiple-resource.json') as f:
+            alert = load(f)
+            resp = handle(alert, LambdaTestContext('test-guardduty-normalizer'))
+            norm = NormalizedOutput(**resp)
+            self.print_pretty(norm, 'raw-s3-multiple-resource.json')
     #         # assert norm.vendorId == 'prisma-cloud'
     #         # assert norm.alertId == 'P-2388888'
     #         # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
@@ -62,21 +62,7 @@ class GuardDutyNormalizerTest(TestCase):
     #         # assert norm.resourceId == 'foobar'
     #         # assert norm.region == 'ap-northeast-1'
     #         # assert norm.resourceContainer == '123456789123'
-    #
-    # def test_direct(self):
-    #     with open('tests/examples/gd-direct.json') as f:
-    #         alert = load(f)
-    #         print(alert)
-    #         resp = handle(alert, LambdaTestContext('test-guardduty-normalizer'))
-    #         norm = NormalizedOutput(**resp)
-    #         self.print_pretty(norm, 'gd-direct.json')
-    #         # assert norm.vendorId == 'prisma-cloud'
-    #         # assert norm.alertId == 'P-2388888'
-    #         # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
-    #         # assert norm.csp == 'aws'
-    #         # assert norm.resourceId == 'foobar'
-    #         # assert norm.region == 'ap-northeast-1'
-    #         # assert norm.resourceContainer == '123456789123'
+
 
     def test_direct_ec2_alpha(self):
         with open('gd-via-sh/tests/examples/direct-ec2-alpha.json') as f:
