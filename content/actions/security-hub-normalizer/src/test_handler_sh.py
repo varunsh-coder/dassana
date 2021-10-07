8c0b88545774cf4dd254259f5228877808b30754
Hossein Khosravani <Hosein@dassana.io>
2021-10-06 22:57:18 -0700

    testing done

--- content/actions/security-hub-normalizer/src/test_handler_sh.py
+++ content/actions/security-hub-normalizer/src/test_handler_sh.py
@@ -39,12 +39,12 @@ class SecurityHubNormalizerTest(TestCase):
             assert norm.region == 'us-east-1'
             assert norm.resourceContainer == '363265257036'
 
-    # def test_eb_ec2(self):
-    #     with open('../tests/examples/ec2_eb.json') as f:
-    #         alert = load(f)
-    #         resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
-    #         norm = NormalizedOutput(**resp)
-    #         self.print_pretty(norm, 'ec2_eb.json')
+    def test_eb_ec2(self):
+        with open('../tests/examples/ec2_eb.json') as f:
+            alert = load(f)
+            resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
+            norm = NormalizedOutput(**resp)
+            self.print_pretty(norm, 'ec2_eb.json')
             assert norm.vendorId == 'aws-config'
             assert norm.alertId == 'arn:aws:securityhub:us-east-1:363265257036:subscription/aws-foundational-security-best-practices/v/1.0.0/APIGateway.4/finding/227ca420-48fa-4df8-ad47-f747521d169a'
             assert norm.vendorPolicy == 'api-gw-associated-with-waf'
@@ -68,16 +68,16 @@ class SecurityHubNormalizerTest(TestCase):
             assert norm.resourceContainer == '363265257036'
 
     def test_raw_s3(self):
-        with open('../tests/examples/s3_eb.json') as f:
+        with open('../tests/examples/s3_raw.json') as f:
             alert = load(f)
             resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
             norm = NormalizedOutput(**resp)
             self.print_pretty(norm, 's3_raw.json')
             assert norm.vendorId == 'aws-config'
-            assert norm.alertId == 'arn:aws:securityhub:us-east-1:363265257036:subscription/aws-foundational-security-best-practices/v/1.0.0/APIGateway.4/finding/227ca420-48fa-4df8-ad47-f747521d169a'
-            assert norm.vendorPolicy == 'api-gw-associated-with-waf'
+            assert norm.alertId == 'arn:aws:securityhub:us-east-1:020747060415:subscription/aws-foundational-security-best-practices/v/1.0.0/S3.2/finding/d0c74471-d0da-40bf-81cd-d871510d49ea'
+            assert norm.vendorPolicy == 's3-bucket-public-read-prohibited'
             assert norm.csp == 'aws'
-            assert norm.resourceId == '6u20vtvjpk'
+            assert norm.resourceId == 'new-fixed-bucket-dassana'
             assert norm.region == 'us-east-1'
-            assert norm.resourceContainer == '363265257036'
+            assert norm.resourceContainer == '020747060415'
 
