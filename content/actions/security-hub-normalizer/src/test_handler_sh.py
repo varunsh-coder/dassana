bf41465ea90c9700f2a00daea4b89953462ba51d
Hossein Khosravani <Hosein@dassana.io>
2021-10-06 21:56:58 -0700

    testing

--- content/actions/security-hub-normalizer/src/test_handler_sh.py
+++ content/actions/security-hub-normalizer/src/test_handler_sh.py
@@ -11,72 +11,73 @@ class SecurityHubNormalizerTest(TestCase):
     def validate_alert_norm(alert):
         print("just an empty shell")
 
-    def print_pretty(self, norm, input):
-        print('')
-        print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
-        print('+++testing input: ' + input)
-        print('+++csp: ' + norm.csp)
-        print("+++resource_container: " + norm.resourceContainer)
-        print("+++region: " + norm.region)
-        print("+++resourceId: " + norm.resourceId)
-        print("+++alertId: " + norm.alertId)
-        print("+++arn: " + norm.arn)
-        print("+++vendorPolicy: " + norm.vendorPolicy)
-        print("+++vendorId: " + norm.vendorId)
+    ### for testing only
+    # def print_pretty(self, norm, input):
+    #     print('')
+    #     print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    #     print('+++testing input: ' + input)
+    #     print('+++csp: ' + norm.csp)
+    #     print("+++resource_container: " + norm.resourceContainer)
+    #     print("+++region: " + norm.region)
+    #     print("+++resourceId: " + norm.resourceId)
+    #     print("+++alertId: " + norm.alertId)
+    #     print("+++arn: " + norm.arn)
+    #     print("+++vendorPolicy: " + norm.vendorPolicy)
+    #     print("+++vendorId: " + norm.vendorId)
 
     def test_eb_api_gateway(self):
-        with open('security-hub-normalizer/tests/examples/example1.json') as f:
+        with open('security-hub-normalizer/tests/examples/apigateway_eb.json') as f:
             alert = load(f)
             resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
             norm = NormalizedOutput(**resp)
-            self.print_pretty(norm, 'example1.json')
-            # assert norm.vendorId == 'prisma-cloud'
-            # assert norm.alertId == 'P-2388888'
-            # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
-            # assert norm.csp == 'aws'
-            # assert norm.resourceId == 'foobar'
-            # assert norm.region == 'ap-northeast-1'
-            # assert norm.resourceContainer == '123456789123'
+            # self.print_pretty(norm, 'apigateway_eb.json')
+            assert norm.vendorId == 'aws-config'
+            assert norm.alertId == 'arn:aws:securityhub:us-east-1:363265257036:subscription/aws-foundational-security-best-practices/v/1.0.0/APIGateway.4/finding/227ca420-48fa-4df8-ad47-f747521d169a'
+            assert norm.vendorPolicy == 'api-gw-associated-with-waf'
+            assert norm.csp == 'aws'
+            assert norm.resourceId == '6u20vtvjpk'
+            assert norm.region == 'us-east-1'
+            assert norm.resourceContainer == '363265257036'
 
     # def test_eb_ec2(self):
-    #     with open('../tests/examples/example2.json') as f:
+    #     with open('../tests/examples/ec2_eb.json') as f:
     #         alert = load(f)
     #         resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
     #         norm = NormalizedOutput(**resp)
-    #         self.print_pretty(norm, 'example2.json')
-            # assert norm.vendorId == 'prisma-cloud'
-            # assert norm.alertId == 'P-2388888'
-            # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
-            # assert norm.csp == 'aws'
-            # assert norm.resourceId == 'foobar'
-            # assert norm.region == 'ap-northeast-1'
-            # assert norm.resourceContainer == '123456789123'
+    #         self.print_pretty(norm, 'ec2_eb.json')
+            assert norm.vendorId == 'aws-config'
+            assert norm.alertId == 'arn:aws:securityhub:us-east-1:363265257036:subscription/aws-foundational-security-best-practices/v/1.0.0/APIGateway.4/finding/227ca420-48fa-4df8-ad47-f747521d169a'
+            assert norm.vendorPolicy == 'api-gw-associated-with-waf'
+            assert norm.csp == 'aws'
+            assert norm.resourceId == '6u20vtvjpk'
+            assert norm.region == 'us-east-1'
+            assert norm.resourceContainer == '363265257036'
 
-    # def test_eb_s3(self):
-    #     with open('../tests/examples/example3.json') as f:
-    #         alert = load(f)
-    #         resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
-    #         norm = NormalizedOutput(**resp)
-    #         self.print_pretty(norm, 'example3.json')
-            # assert norm.vendorId == 'prisma-cloud'
-            # assert norm.alertId == 'P-2388888'
-            # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
-            # assert norm.csp == 'aws'
-            # assert norm.resourceId == 'foobar'
-            # assert norm.region == 'ap-northeast-1'
-            # assert norm.resourceContainer == '123456789123'
+    def test_eb_s3(self):
+        with open('../tests/examples/s3_eb.json') as f:
+            alert = load(f)
+            resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
+            norm = NormalizedOutput(**resp)
+            self.print_pretty(norm, 's3_eb.json')
+            assert norm.vendorId == 'aws-config'
+            assert norm.alertId == 'arn:aws:securityhub:us-east-1:363265257036:subscription/aws-foundational-security-best-practices/v/1.0.0/APIGateway.4/finding/227ca420-48fa-4df8-ad47-f747521d169a'
+            assert norm.vendorPolicy == 'api-gw-associated-with-waf'
+            assert norm.csp == 'aws'
+            assert norm.resourceId == '6u20vtvjpk'
+            assert norm.region == 'us-east-1'
+            assert norm.resourceContainer == '363265257036'
 
-    # def test_raw_s3(self):
-    #     with open('../tests/examples/example3.json') as f:
-    #         alert = load(f)
-    #         resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
-    #         norm = NormalizedOutput(**resp)
-    #         self.print_pretty(norm, 'example4.json')
-            # assert norm.vendorId == 'prisma-cloud'
-            # assert norm.alertId == 'P-2388888'
-            # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
-            # assert norm.csp == 'aws'
-            # assert norm.resourceId == 'foobar'
-            # assert norm.region == 'ap-northeast-1'
-            # assert norm.resourceContainer == '123456789123'
+    def test_raw_s3(self):
+        with open('../tests/examples/s3_eb.json') as f:
+            alert = load(f)
+            resp = handle(alert, LambdaTestContext('security-hub-normalizer'))
+            norm = NormalizedOutput(**resp)
+            self.print_pretty(norm, 's3_raw.json')
+            assert norm.vendorId == 'aws-config'
+            assert norm.alertId == 'arn:aws:securityhub:us-east-1:363265257036:subscription/aws-foundational-security-best-practices/v/1.0.0/APIGateway.4/finding/227ca420-48fa-4df8-ad47-f747521d169a'
+            assert norm.vendorPolicy == 'api-gw-associated-with-waf'
+            assert norm.csp == 'aws'
+            assert norm.resourceId == '6u20vtvjpk'
+            assert norm.region == 'us-east-1'
+            assert norm.resourceContainer == '363265257036'
 
