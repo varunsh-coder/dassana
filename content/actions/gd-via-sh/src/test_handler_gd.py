a1fcfa23edfa266a8da4bb40ee1a1af06eea2094
Hossein Khosravani <Hosein@dassana.io>
2021-10-06 21:57:32 -0700

    testing

--- content/actions/gd-via-sh/src/test_handler_gd.py
+++ content/actions/gd-via-sh/src/test_handler_gd.py
@@ -54,20 +54,20 @@ class GuardDutyNormalizerTest(TestCase):
             assert norm.region == 'us-east-1'
             assert norm.resourceContainer == '020747060415'
 
-
+    # until CORN issue resolves
     # def test_direct_ec2_alpha(self):
     #     with open('gd-via-sh/tests/examples/direct-ec2-alpha.json') as f:
     #         alert = load(f)
     #         resp = handle(alert, LambdaTestContext('test-guardduty-normalizer'))
     #         norm = NormalizedOutput(**resp)
     #         self.print_pretty(norm, 'direct-ec2-alpha.json')
-    #         # assert norm.vendorId == 'guardduty'
-    #         # assert norm.alertId == 'P-2388888'
-    #         # assert norm.vendorPolicy == '34064d53-1fd1-42e6-b075-45dce495caca'
-    #         # assert norm.csp == 'aws'
-    #         # assert norm.resourceId == 'foobar'
-    #         # assert norm.region == 'ap-northeast-1'
-    #         # assert norm.resourceContainer == '123456789123'
+    #         assert norm.vendorId == 'guardduty'
+    #         assert norm.alertId == 'arn:aws:guardduty:us-east-1:020747060415:detector/a2bdf2d15d3f3187077af621af3e234d/finding/a0bdfff401df680ce958b027abe1c311'
+    #         assert norm.vendorPolicy == 'UnauthorizedAccess:EC2/SSHBruteForce'
+    #         assert norm.csp == 'aws'
+    #         assert norm.resourceId == 'i-0bc95ef8e97a7d331'
+    #         assert norm.region == 'us-east-1'
+    #         assert norm.resourceContainer == '020747060415'
 
     def test_raw_ec2_alpha(self):
         with open('gd-via-sh/tests/examples/raw-ec2-alpha.json') as f:
